38755a558b27df42d90655f998e8be866c174bea
saurabhkaul77 <77skaul@gmail.com>
2024-12-07 12:12:29 +0000

    datatype summary

---
+++ 01_basic/datatype_summery.js
@@ -0,0 +1,27 @@
+// pritimive
+
+
+// ==> String
+// ==> Number
+// ==>Boolean
+// ==>null
+// ==>undefined
+// ==>Symbol
+// ==>BigInt
+
+
+// non-primitive  (reference)
+
+// ==>array
+// ==>object
+// ==>function
+
+
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id===anotherId)
+
+
