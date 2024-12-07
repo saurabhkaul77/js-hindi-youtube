419e72380a65abb9a10610e47590d1759ebfbee0
saurabhkaul77 <77skaul@gmail.com>
2024-12-07 11:46:02 +0000

    operatioins

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -2,12 +2,30 @@
 let score = "40ava"
 
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let value = Number(score)
-console.log( value)    // NaN
+// console.log( value)   ==>   // NaN
 
 
 // "33" => 33
 // "33abc" => NaN
+
+
+// operation
+
+let a = 4;
+let neg_a = -a
+// console.log(2%3)
+
+// console.log(2 + "3")
+// console.log("4" + 3)
+// console.log("1"+ 3+4)
+// console.log(2 + 3+ "3")
+
+// console.log(+true)
+
+
+
+
