13a16e7a072d51a3879cab6c6c3a7f784ff44089
saurabhkaul77 <77skaul@gmail.com>
2024-12-07 13:57:55 +0000

    Math and num

---
+++ 01_basic/06_Number_and_Math.js
@@ -0,0 +1,23 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(3))
+const balance1 = 13.8
+// console.log(balance1.toPrecision(3))
+
+// +++++++++++++++++++++++++++++++++++++++++++++
+
+const a = 132.53
+// console.log(Math.round(a))
+// console.log(Math.floor(a))
+// console.log(Math.ceil(a))
+
+// console.log((Math.random()*10)+1 )
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min +1))+10)
