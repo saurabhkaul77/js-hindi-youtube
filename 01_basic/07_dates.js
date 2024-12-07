83fa4a775c08234962da296b6292279f106a1b27
saurabhkaul77 <77skaul@gmail.com>
2024-12-07 14:22:37 +0000

    dates

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,29 @@
+let date = new Date()
+// console.log(date.toString())
+
+// console.log(date.toDateString())
+// console.log(date.toLocaleDateString())
+
+
+let myDate = new Date(2030, 0, 23)
+let myDate1 = new Date(2030, 0, 23,4,3)
+
+// console.log(myDate.toLocaleString())
+// console.log(myDate1.toDateString())
+
+
+let timeStamo = Date.now()
+// console.log(timeStamo)
+
+let dateTime  = new Date("01-02-2025") 
+// console.log(dateTime.getTime());
+
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getDay())
+console.log(newDate.getMonth()+1)
+
+
+
