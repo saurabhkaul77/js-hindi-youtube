ba68d28bd95f28ed1382493db01df6f37571eae5
saurabhkaul77 <77skaul@gmail.com>
2024-12-08 17:13:12 +0000

    function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,36 @@
+// function add(parameter1, parameter2){
+//     console.log(parameter1+parameter2)
+// }
+// add(arguments)
+
+
+
+
+
+                              
+// function login(username = "kaul"){
+//     if(!username){
+//         console.log("Enter a username")
+//             return;
+//     }
+//     return `${username} just login in`
+// }
+// console.log(login("Saurabh"));
+
+
+function calculate(a,b,...num){
+    return num;
+}
+// console.log(calculate(2,3,45,6,8))
+
+const user = {
+    username: "KAUL",
+    price: 99
+}
+
+// function handelObj (anyObj){
+//     console.log(`username ${anyObj.username} and price${anyObj.price}`)
+// }
+// handelObj(user)
+
+ 
